586b7e32513fd715e93434dce318fa04ff505591
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-12-05 01:17:30 +0100

    Add files via upload

---
+++ naslijedjivanjeBeakovicPaolo/naslijedjivanjeBeakovicPaolo/ComboBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naslijedjivanjeBeakovicPaolo
+{
+    class ComboBox 
+    {
+        public void Metoda()
+        {
+            string output;
+            output = "Naziv Metode je Metoda";
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ naslijedjivanjeBeakovicPaolo/naslijedjivanjeBeakovicPaolo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naslijedjivanjeBeakovicPaolo
+{
+    interface IControl
+    {
+        void Metoda();
+        int Brojac { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListBox x = new ListBox();
+            x.Metoda();
+            ComboBox y = new ComboBox();
+            y.Metoda();
+            Console.ReadKey();
+
+        }
+    }
+}
